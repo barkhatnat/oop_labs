14c39f5e30f8690017eb2c619742adc368a86703
Natalia Barkhatova <natabarkhatova@yandex.ru>
2023-09-15 20:11:49 +0300

    enum denominations

---
+++ lab1/Enums/AllDenominatons.cs
@@ -0,0 +1,18 @@
+﻿namespace lab1.Enums;
+
+public enum AllDenominations
+{
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
---
+++ lab1/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using lab1.Enums;
+
+namespace lab1.Models;
+
+public class Card
+{
+    private AllDenominations Denomination { get; set; }
+    private AllDenominations Suit { get; set; }
+    private bool IsOpen { get; set; }
+    
+    public Card(AllDenominations denomination, AllDenominations suit, bool isOpen) 
+    {
+         
+
+    }
+
+}
